13a17a31908bd31198b7bd6233760962feb31b56
THEAsamika <134443801+THEAsamika@users.noreply.github.com>
2023-06-19 17:29:40 -0400

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -33,7 +33,9 @@ class MainMenuState extends MusicBeatState
 	private var camAchievement:FlxCamera;
 	
 	var optionShit:Array<String> = [
+		'story_mode',
 		'freeplay',
+		#if MODS_ALLOWED 'mods', #end
 		'credits',
 		'options'
 	];
@@ -129,7 +131,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine Modded v" + psychEngineVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
